f12efb0e62cd165ff4738001de7b618185118507
Mr-Aylex <carmonealexandre@gmail.com>
2020-02-21 14:45:42 +0100

    ajout des commentaires

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Tache;
 use App\Form\TacheType;
-use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,48 +11,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TacheController extends AbstractController
 {
+    //page principale
     /**
      * @Route("/", name="home")
      */
     public function home()
     {
-        $repository = $this->getDoctrine()->getRepository(Tache::class);
-        $tache = $repository->findby([],['date_creation' => 'ASC']);
+        $repository = $this->getDoctrine()->getRepository(Tache::class);//on recupère tous les éléments de la base de donnée
+        $tache = $repository->findby([],['date_creation' => 'ASC']);//on met par default le l'ordre en croissant
         return $this->render('tache/homes.html.twig',[
             "taches" => $tache,
-            "option" => $filtre = null,
-            "option_date" =>  $trie = 'ASC'
+            "option" => $filtre = null,//aucun filtre par default
+            "option_date" =>  $trie = 'ASC'//on met par default le l'ordre croissant selectioner
         ]);
     }
+    //route pour trier les taches
     /**
      * @Route("/trie", name="trie")
      */
-    public function tacheTrier(Request $request, $trie = null, $filtre = null )
+    public function tacheTrier(Request $request, $trie = 'ASC', $filtre = null )
     {
         $repository = $this->getDoctrine()->getRepository(Tache::class);
-        $filtre = $request->get('filtre');
-        $trie = $request->get('ordre');
-        if($filtre === null)
+        $filtre = $request->get('filtre');//on recupère la valeur filtre envoyer par le POST 
+        $trie = $request->get('ordre');//on recupère la valeur ordre envoyer par le POST
+        if($filtre === null)//Si il n'y pas de filtre alors
         {
             $tache = $repository->findBy(
-                [],
-                ['date_creation' => $trie]
+                [],//sans filtre
+                ['date_creation' => $trie]//on filre par date de création ASC
             );
         }
-        else
+        else//si il y a des filtres
         {
             if($trie == 'ASC')
             {
                 $tache = $repository->findBy(
-                    ['statut' => $filtre],
-                    ['date_creation' => 'ASC']
+                    ['statut' => $filtre],//on filtre par le statut
+                    ['date_creation' => 'ASC']//et on met dans l'ordre croissant les taches par leur dates de création
                 );
             }
             else
             {
                 $tache = $repository->findBy(
-                    ['statut' => $filtre],
-                    ['date_creation' => 'DESC']
+                    ['statut' => $filtre],//on filtre par le statut
+                    ['date_creation' => 'DESC']//et on met dans l'ordre décroissant les taches par leur dates de création
                 );
             }
             // $tache = $repository->findBy(
@@ -64,50 +65,56 @@ class TacheController extends AbstractController
 
             // );
         }
-        return $this->render('tache/homes.html.twig',[
+        return $this->render('tache/homes.html.twig',[//on applique les filtres et les tries dans la page
             "taches" => $tache,
-            "option" => $filtre,
-            "option_date" =>  $trie
+            "option" => $filtre,//variable qui met l'élement selectioné précédement en selected
+            "option_date" =>  $trie//variable qui met l'élement selectioné précédement en selected
         ]);
     }
+    /*
+    Route pour modifier et supprimer une tache
+    */
     /**
      * @Route("/nouvelle_tache", name="nouvelle_tache")
      * @Route("/modif/{id}", name="modif_tache" , methods = "GET|POST")
      */
     public function Ajout_modif_tache(Tache $tache = null, Request $request, EntityManagerInterface $entityManagerInterface)
     {
-        if(!$tache)
+        if(!$tache)//Si la tache est null alors on en crée une 
         {
             $tache = new Tache();
-            $tache->setDateCreation(new \DateTime('now'));
+            $tache->setDateCreation(new \DateTime('now'));//on prérempli la date et l'heure pour qu'elle soit corresponde à l'actuelle
         }
-        $tache->setMiseAJour(new \DateTime('now'));
-        $form = $this->createForm(TacheType::class,$tache);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
+        $tache->setMiseAJour(new \DateTime('now'));// dans le cas de la modification et de la création d'une tache on met une date de mise a jours actuelle
+        $form = $this->createForm(TacheType::class,$tache);// on envoie le formulaire
+        $form->handleRequest($request);// on récupere la requète
+        if($form->isSubmitted() && $form->isValid())//si le form est valider
         {
             $modif = $tache->getId() !== null;
-            $entityManagerInterface->persist($tache);
-            $entityManagerInterface->flush();
-            $this->addFlash("success", ($modif) ?"La modification a été effectuée" : "Le nouveau Tache a été créé");
-            return $this->redirectToRoute("home");
+            $entityManagerInterface->persist($tache);// on rentre toute les infos
+            $entityManagerInterface->flush();//on met à jours la base de donnée
+            $this->addFlash("success", ($modif) ?"La modification a été effectuée" : "Le nouveau Tache a été créé");//un message est affiché pour confirmer la modification la création de la tahce
+            return $this->redirectToRoute("home");//on redirige vers la page principale
         }
-        return $this->render('tache/tacheFormulaire.html.twig', [
+        return $this->render('tache/tacheFormulaire.html.twig', [// si il n'y pas de requete valider on envoie le formulaire avec la nouvelle tache au formulaire ou la tache modifier dans la page du formulaire
             "tache" => $tache,
             "form" => $form->createView(),
             "ismodification" => $tache->getId() !== null
         ]);
     }
+    /*
+    Route pour suprimer une tache
+    */
     /**
      * @Route("/{id}", name="suppression", methods = "delete")
      */
-    public function suppr_personnages(Tache $tache, Request $request, EntityManagerInterface $entityManagerInterface)
+    public function suppr_tache(Tache $tache, Request $request, EntityManagerInterface $entityManagerInterface)
     {
-        if($this->isCsrfTokenValid("SUP". $tache->getId(), $request->get('_token')))
+        if($this->isCsrfTokenValid("SUP". $tache->getId(), $request->get('_token')))//On verifie si le code envoyer par la page twig est identique pour eviter qu'un utilisateur face une suppression non désirer
         {
             $entityManagerInterface->remove($tache);
-            $entityManagerInterface->flush();
-            $this->addFlash("success", "La suppression a été effectuée");
+            $entityManagerInterface->flush();//On supprime et on actualise la base de donnée
+            $this->addFlash("success", "La suppression a été effectuée");//un message est affiché pour confirmer la suppression
             return $this->redirectToRoute("home");
         }
         
